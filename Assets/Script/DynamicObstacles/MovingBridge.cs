6fbf5276bc44c767c153a75f0d1f628dff724a8c
yuzu1012 <yuzusonyuzuha@gmail.com>
2025-10-13 15:45:06 +0900

    自分のシーンの変更 橋にプレイヤーが乗ったら、プレイヤーと橋が一緒に動く スクリプトの修正

--- Assets/Script/DynamicObstacles/MovingBridge.cs
+++ Assets/Script/DynamicObstacles/MovingBridge.cs
@@ -6,10 +6,10 @@ public class MovingBridge : MonoBehaviour
     public BridgeSpawner bridgeSpawner;
 
     [Header("動く速さ")]
-    [SerializeField] float moveSpeed;
+    [SerializeField] private float moveSpeed;
 
     [Header("消滅時間")]
-    [SerializeField] float destoryTime;
+    [SerializeField] private float destoryTime;
     private float destoryTimer;
 
     private Rigidbody rb;
@@ -23,6 +23,7 @@ public class MovingBridge : MonoBehaviour
     {
         rb.linearVelocity = new Vector3(moveSpeed, 0f, 0f);
 
+        //消滅時間が経ったら、橋を消す
         destoryTimer += Time.fixedDeltaTime;
         if(destoryTimer >= destoryTime)
         {
@@ -30,26 +31,22 @@ public class MovingBridge : MonoBehaviour
         }
     }
 
-    void OnBecameInvisible()
-    {
-        //bridgeSpawner.Release(this.gameObject);
-    }
-
-
     //浮き沈みするようにする
     private void OnCollisionEnter(Collision collision)
     {
+        //プレイヤーが橋に乗った時、プレイヤーを橋の子オブジェクトにする
         if (collision.gameObject.CompareTag("Player"))
         {
-            
+            collision.gameObject.transform.SetParent(transform);
         }
     }
 
     private void OnCollisionExit(Collision collision)
     {
+        //プレイヤーを橋の子オブジェクトじゃなくする
         if (collision.gameObject.CompareTag("Player"))
         {
-
+            collision.gameObject.transform.SetParent(null);
         }
     }
 }
